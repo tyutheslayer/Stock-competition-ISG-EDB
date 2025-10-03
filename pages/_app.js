9ebae191759fd43564eba4bbf526cb3f05d892b5
tyutheslayer <00.3li07.00@gmail.com>
2025-10-03 21:19:41 +0200

    changement 3D Design etc

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // pages/_app.js
 import { SessionProvider } from "next-auth/react";
 
+// ⚠️ Mets le BON chemin vers ton CSS global :
+//  - si ton fichier s'appelle styles/global.css  -> garde cette ligne
+//  - si c'est styles/globals.css                 -> remplace par "../styles/globals.css"
+import "../styles/global.css";
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
