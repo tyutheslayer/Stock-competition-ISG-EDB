0dee06c370f2ba94021ace70dc8c0ac34f2a93d7
tyutheslayer <00.3li07.00@gmail.com>
2025-10-03 19:40:50 +0200

    changement 3D Design etc

--- components/PageShell.jsx
+++ components/PageShell.jsx
@@ -1,3 +1,4 @@
+// components/PageShell.jsx
 import clsx from "clsx";
 import NavBar from "./NavBar";
 import NeonBackground3D from "./NeonBackground3D";
@@ -5,20 +6,20 @@ import NeonBackground3D from "./NeonBackground3D";
 export default function PageShell({ children, className = "" }) {
   return (
     <div className="relative min-h-screen">
-      {/* Dégradé de fond */}
+      {/* Couche dégradée très douce, SOUS la 3D */}
       <div className="app-gradient" />
 
-      {/* Canvas 3D */}
-      <NeonBackground3D className="-z-20" />
+      {/* Fond 3D – non cliquable, passe sous le contenu */}
+      <NeonBackground3D className="-z-20 pointer-events-none" />
 
-      {/* Contenu */}
+      {/* Contenu au-dessus */}
       <header className="relative z-10">
         <NavBar />
       </header>
 
       <main
         className={clsx(
-          "relative z-10 max-w-7xl mx-auto px-4 sm:px-6 py-6",
+          "relative z-10 max-w-[1280px] mx-auto px-5 md:px-6 py-6",
           className
         )}
       >
